c189e3a331d613152c0e8e01d3669d4e7d84fae4
Detjon <detjon.mataj@gmail.com>
2022-07-21 00:05:40 +0200

    Added Music COG

--- utils/PyBot.py
+++ utils/PyBot.py
@@ -1,3 +1,4 @@
+from cogs.music.Music import Music
 from utils.DiscordBot import DiscordBot
 from cogs.main.Main import Main
 
@@ -9,4 +10,5 @@ class PyBot(DiscordBot):
     def __init__(self, token: str, prefix: str):
         super(PyBot, self).__init__(command_prefix=prefix)
         self.add_cog(Main(super()))
+        self.add_cog(Music(super()))
         self.run(token)
